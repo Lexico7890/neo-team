8394c938b89d0f4c71156baddf858d0ead73887a
oscar.casas <oscar.casas@grupokonecta.com>
2024-01-02 22:27:29 -0500

    Add endpoint for show general information tournament

---
+++ app/types/function/tournamentGeneral.ts
@@ -0,0 +1,4 @@
+import { type Database } from '../database'
+
+type TournamentGeneralEntity = Database['public']['Functions']['get_tournament_count_info']['Returns']
+export type TournamentGeneral = TournamentGeneralEntity
